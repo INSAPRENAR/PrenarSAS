cdfb8dbc74ef09c542cbfc22dc43fc435c0c2176
danielvillota <danielfernandovillota@gmail.com>
2024-12-03 10:45:09 -0500

    cambio de funcion create al serializador user

--- api_prenar/views/registerView.py
+++ api_prenar/views/registerView.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from api_prenar.serializers.userSerializers import UserSerializer
 
 class Register(APIView):
@@ -7,13 +8,6 @@ class Register(APIView):
         serializer=UserSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
     
-    def create(self, validated_data):
-        password = validated_data.pop('password', None)
-        instance = self.Meta.model(**validated_data)
-        if password is not None:
-            instance.set_password(password)
-        instance.save()
-        return instance
 
