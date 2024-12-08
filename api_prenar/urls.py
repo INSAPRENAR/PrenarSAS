413116df471f389a47810ebd880084ed1810f1d9
danielvillota <danielfernandovillota@gmail.com>
2024-12-07 19:48:33 -0500

    creacion actualizacion y eliminacion de usuario

--- api_prenar/urls.py
+++ api_prenar/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Register, loginView, UsersView, LogoutView, ListUserView
+from .views import Register, loginView, UsersView, LogoutView, ListUserView, UserView, UserDetail
 
 urlpatterns = [
     path('register', Register.as_view(), name='register'),
@@ -7,4 +7,7 @@ urlpatterns = [
     path('home', UsersView.as_view(), name='home'),
     path('logout', LogoutView.as_view(), name='logout'),
     path('listUsers', ListUserView.as_view(), name='listUsers'),
+    path('user/<int:user_id>', UserView.as_view(), name='user_detail'),
+    path('user/<int:user_id>/update', UserDetail.as_view(), name='user-update'),
+    path('user/<int:user_id>/delete', UserDetail.as_view(), name='user-delete'),
 ]
