a825032dd2e989e35b75189d076ce139d6e1fc0a
danielvillota <danielfernandovillota@gmail.com>
2024-12-08 17:10:49 -0500

    creacion del modelo cliente

---
+++ api_prenar/models/cliente.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Cliente(models.Model):
+    id=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=255)
+    phone=models.CharField(max_length=15, null=True, blank=True)
+    address=models.CharField(null=True, blank=True)
+    email=models.EmailField(unique=True)
+    identification=models.CharField(unique=True)
+    identification_asesor=models.CharField()
+    email_user=models.EmailField()
+    registration_date=models.DateField(auto_now_add=True)
