79c1894a6d344ef99380b256604a5e9adf360a10
danielvillota <danielfernandovillota@gmail.com>
2024-12-10 08:50:23 -0500

    creacion modelo inventario

---
+++ api_prenar/models/inventario.py
@@ -0,0 +1,20 @@
+from django.db import models
+from api_prenar.models import Pedido
+
+class Inventario(models.Model):
+    id=models.AutoField(primary_key=True)
+    #llave foranea
+    inventory_date=models.DateField()
+    id_pedido=models.OneToOneField(Pedido, on_delete=models.CASCADE)
+    number_upload=models.CharField()
+    conformal_production=models.IntegerField()
+    not_comformal_production=models.IntegerField()
+    comformal_output=models.IntegerField()
+    not_comformal_output=models.IntegerField()
+    total_production=models.IntegerField()
+    total_output=models.IntegerField()
+    balance=models.IntegerField()
+    email_user=models.EmailField()
+    registration_date=models.DateField(auto_now_add=True)
+
+
