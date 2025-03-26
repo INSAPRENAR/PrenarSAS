6b10d806c7e3b3b627965923169f6c69dc77774c
danielvillota <danielfernandovillota@gmail.com>
2025-03-26 18:57:30 -0500

    modificacion tipo de datos en campos en diferentes de modelos

--- api_prenar/models/categoria_material.py
+++ api_prenar/models/categoria_material.py
@@ -4,4 +4,4 @@ class CategoriaMaterial(models.Model):
     id=models.AutoField(primary_key=True)
     name=models.CharField(max_length=255)
     color=models.CharField(max_length=255, null=True, blank=True)
-    stock_quantity=models.IntegerField(default=0)
+    stock_quantity=models.FloatField(default=0.0)
--- api_prenar/models/producto.py
+++ api_prenar/models/producto.py
@@ -4,7 +4,7 @@ class Producto(models.Model):
     id=models.AutoField(primary_key=True)
     product_code=models.CharField(unique=True)
     name=models.CharField(max_length=255)
-    description=models.CharField(max_length=255)
+    description=models.CharField(max_length=255, null=True, blank=True)
     unit_price=models.FloatField()
     discounted_unit_price=models.FloatField()
     color=models.CharField(max_length=255)
