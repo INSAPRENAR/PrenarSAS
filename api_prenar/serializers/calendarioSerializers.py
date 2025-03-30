2c8973d0406b90563d0e6386d9def5ef310ab46e
danielvillota <danielfernandovillota@gmail.com>
2025-03-30 10:55:59 -0500

    modificacion de atributos en modelo y se agrego campos en serializers

--- api_prenar/serializers/calendarioSerializers.py
+++ api_prenar/serializers/calendarioSerializers.py
@@ -9,21 +9,27 @@ class CalendarioSerializer(serializers.ModelSerializer):
 class CalendarioTipo1Serializer(serializers.ModelSerializer):
     order_code = serializers.SerializerMethodField()
     name = serializers.SerializerMethodField()
+    color_producto=serializers.SerializerMethodField()
     class Meta:
         model = Calendario
-        fields = ['id','calendar_date', 'expected_date', 'id_pedido','order_code', 'id_producto','name', 'amount', 'machine', 'state', 'observation']
+        fields = ['id','calendar_date', 'expected_date', 'id_pedido','order_code', 'id_producto','name','color_producto', 'amount', 'machine', 'state', 'observation']
     def get_order_code(self, obj):
         return obj.id_pedido.order_code if obj.id_pedido else None
     def get_name(self, obj):
         return obj.id_producto.name if obj.id_producto else None
+    def get_color_producto(self, obj):
+        return obj.id_producto.color if obj.id_producto else None
 
 class CalendarioTipo2Serializer(serializers.ModelSerializer):
     order_code = serializers.SerializerMethodField()
     name = serializers.SerializerMethodField()
+    color_producto=serializers.SerializerMethodField()
     class Meta:
         model = Calendario
-        fields = ['id','calendar_date', 'expected_date', 'id_pedido', 'order_code', 'id_producto','name', 'amount', 'dispatch_time', 'state', 'observation']
+        fields = ['id','calendar_date', 'expected_date', 'id_pedido', 'order_code', 'id_producto','name','color_producto', 'amount', 'dispatch_time', 'state', 'observation']
     def get_order_code(self, obj):
         return obj.id_pedido.order_code if obj.id_pedido else None
     def get_name(self, obj):
         return obj.id_producto.name if obj.id_producto else None
+    def get_color_producto(self, obj):
+        return obj.id_producto.color if obj.id_producto else None
