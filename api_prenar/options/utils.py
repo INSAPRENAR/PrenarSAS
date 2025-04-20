a9a8544d09f4db7b91ee2a5654e74d084dd86b33
danielvillota <danielfernandovillota@gmail.com>
2025-04-20 18:42:52 -0500

    modificacion campo modelo consumo materiales

--- api_prenar/options/utils.py
+++ api_prenar/options/utils.py
@@ -7,6 +7,6 @@ def get_total_almacen(referencia):
     try:
         # Busca el producto en la base de datos por referencia
         producto = Producto.objects.get(id=referencia)
-        return producto.warehouse_quantity  # Ajusta si el campo del almacén tiene otro nombre
+        return producto.warehouse_quantity_conforme  # Ajusta si el campo del almacén tiene otro nombre
     except Producto.DoesNotExist:
         return 0  # Si no existe el producto, devuelve 0
