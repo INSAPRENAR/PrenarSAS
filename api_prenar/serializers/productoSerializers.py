3cbf8f1abcc7565d4f761d9fdb0018d759dcb5d6
danielvillota <danielfernandovillota@gmail.com>
2025-04-27 19:57:55 -0500

    modificacion respuestas y validaciones

--- api_prenar/serializers/productoSerializers.py
+++ api_prenar/serializers/productoSerializers.py
@@ -10,8 +10,8 @@ class ProductoSerializer(serializers.ModelSerializer):
         """
         Validaciones personalizadas para los datos del producto.
         """
-        if data.get('unit_price', 0) <= 0:
-            raise serializers.ValidationError("El precio unitario debe ser mayor a 0.")
+        if data.get('unit_price', 0) < 0:
+            raise serializers.ValidationError("El precio unitario no puede ser negativo.")
         if data.get('discounted_unit_price', 0) < 0:
             raise serializers.ValidationError("El precio con descuento no puede ser negativo.")
         if data.get('warehouse_quantity_conforme', 0) < 0:
